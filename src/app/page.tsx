3716eb69898a18f281ac71bec1fbf8670b41be70
Matthew <matthewoo16@gmail.com>
2023-07-13 19:03:26 -0400

    Adjusted Page to use the Pagination, usePagination, and BlogRecord component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@
 
 import { Post } from '@/model/Post';
 import axios from 'axios';
-import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import { BlogRecord } from './components/blog_record/blogRecord';
+import { Pagination } from './components/pagination/pagination';
+import usePagination from './hooks/usePagination';
 
 const BlogPosts: React.FC = () => {
+  const postPerPage = 5;
+
   const [posts, setPosts] = useState<Post[]>([]);
+  const [displayPost, setDisplayPost] = useState<Post[]>([]);
+  const [inPost, setInPost] = useState<number | null>(null);;
+  const { currentPage, totalPages, goToPage, goToNextPage, goToPreviousPage } = usePagination(postPerPage, posts.length);
+
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -22,24 +29,70 @@ const BlogPosts: React.FC = () => {
     fetchPosts();
   }, []);
 
-  return (
-    <div>
-      <div>Blog Posts</div>
-      <table>
-        <tr>
-          <th></th>
-          <th>Title</th>
-          <th>Body</th>
-        </tr>
-        {
-          posts.map((post) => (
-            <BlogRecord
-              post={post} />
-          ))
-        }
-      </table>
+  useEffect(() => {
+    handleDisplayPost();
+  }, [posts, inPost, currentPage])
 
-    </div>
+  const handleDisplayPost = () => {
+    if (inPost) {
+      const selectedPost = posts.find((post) => post.id === inPost);
+      setDisplayPost(selectedPost ? [selectedPost] : []);
+    } else {
+      setDisplayPost(displayCurrentPagePost(currentPage));
+    }
+  }
+
+  const displayCurrentPagePost = (currentPage: number) => {
+    const result = posts.filter(post =>
+      post.id > (currentPage - 1) * postPerPage  //the first 
+      &&
+      post.id <= currentPage * postPerPage);
+    return result;
+  }
+
+  return (
+    <main className="flex">
+      <div className='bg-yellow-400 p-4'>
+        <div className='text-white text-center font-serif text-2xl font-semibold p-3 flex'>
+          {
+            inPost &&
+            <button onClick={() => setInPost(null)}>
+              {'<'}
+            </button>
+          }
+          <div className='grow'> {inPost ? 'Post Details' : 'Blog Posts'}</div>
+        </div>
+        <table className="table-fixed border border-collapse">
+          <thead>
+            <tr>
+              <th className="bg-green-500 border-4 border-yellow-400  p-4"></th>
+              <th className="bg-green-500 border-4 border-yellow-400  p-4">Title</th>
+              <th className="bg-green-500 border-4 border-yellow-400  p-4">Body</th>
+            </tr>
+          </thead>
+          {
+            displayPost.map((post) => (
+              <BlogRecord
+                key={post.id}
+                post={post}
+                inPost={inPost}
+                setInPost={setInPost} />
+            ))
+          }
+        </table>
+
+        {
+          !inPost &&
+          <Pagination
+            goToPreviousPage={goToPreviousPage}
+            currentPage={currentPage}
+            goToNextPage={goToNextPage}
+            totalPages={totalPages}
+          />
+        }
+
+      </div>
+    </main>
   );
 };
 
